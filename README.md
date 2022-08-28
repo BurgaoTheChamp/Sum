d0e4b460dd93c598b54698acf707c01945ed4d93
BurgaoTheChamp <94881470+BurgaoTheChamp@users.noreply.github.com>
2022-08-28 19:45:09 -0300

    Added bestSum and README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Target sum
+
+Comparing the efficiency of memoization on sum algos
+
+# Methods
+
+canSum -> checks if the sum can be done 
+howSum -> returns one of the combinations that can be done 
+bestSum -> returns the best combination that can be done 
--- Sum.cpp
+++ Sum.cpp
@@ -45,6 +45,33 @@ std::vector<LL> howSum(const LL &targetSum, const std::vector<LL> &array){
     return {-1}; 
 }
 
+std::vector<LL> bestSum(const LL &targetSum, const std::vector<LL> &array){
+    if (targetSum < 0)
+    {
+        return {-1};
+    }
+
+    std::vector<LL> shortest = {-1};
+
+    for (int i = 0; i < array.size(); i++)
+    {
+        if (!(targetSum - array[i]))
+        {
+            return {array[i]};
+        }
+        std::vector<LL> result = bestSum(targetSum - array[i], array);
+        if(result[0] != -1){
+            result.push_back(array[i]);
+            if (shortest[0] == -1 || result.size() < shortest.size() )
+            {
+                shortest.pop_back();
+                shortest = result;
+            }          
+        }         
+    }
+    return shortest; 
+}
+
 bool canSum_hashed(const LL &targetSum, const std::vector<LL> &array){
 
     static std::unordered_map<LL, bool> container;
@@ -104,6 +131,41 @@ std::vector<LL> howSum_hashed(const LL &targetSum, const std::vector<LL> &array)
     return {-1}; 
 }
 
+std::vector<LL> bestSum_hashed(const LL &targetSum, const std::vector<LL> &array){
+
+    static std::unordered_map<LL, std::vector<LL>> cache1;
+
+    if (targetSum < 0)
+    {
+        return {-1};
+    }
+    if (cache1.count(targetSum))
+    {
+        return cache1[targetSum];
+    }
+
+    std::vector<LL> shortest = {-1};
+
+    for (int i = 0; i < array.size(); i++)
+    {
+        if (!(targetSum - array[i]))
+        {
+            return {array[i]};
+        }
+        std::vector<LL> result = bestSum_hashed(targetSum - array[i], array);
+        if(result[0] != -1){
+            result.push_back(array[i]);
+            if (shortest[0] == -1 || result.size() < shortest.size() )
+            {
+                shortest.pop_back();
+                shortest = result;
+            }          
+        }         
+    }
+    cache1[targetSum] = shortest;
+    return shortest; 
+}
+
 int main(){
 
     bool keep_going = true;
@@ -153,6 +215,23 @@ int main(){
 
         std::cout << "Elapsed time was: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_howsum_hashed - start_howsum_hashed).count() << " ms" << std::endl;
 
+        auto start_bestsum_hashed = std::chrono::steady_clock::now();
+
+        std::cout << "The Best combination (hashed): \n";
+
+        auto result_best_hashed = bestSum_hashed(t, numbers);
+
+        for (uint64_t i = 0; i < result_best_hashed.size(); i++)
+        {
+            std::cout << result_best_hashed[i] << " ";
+        }
+        
+        std::cout << "\n";
+
+        auto end_bestsum_hashed = std::chrono::steady_clock::now();
+
+        std::cout << "Elapsed time was: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_bestsum_hashed - start_bestsum_hashed).count() << " ms" << std::endl;
+
         auto start = std::chrono::steady_clock::now();
 
         std::cout << "Is it possible to sum: " << canSum(t, numbers) << std::endl;
@@ -177,6 +256,23 @@ int main(){
         auto end_howsum = std::chrono::steady_clock::now();
 
         std::cout << "Elapsed time was: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_howsum - start_howsum).count() << " ms" << std::endl;
+
+        auto start_bestsum = std::chrono::steady_clock::now();
+
+        std::cout << "The Best combination: \n";
+
+        auto result_best = bestSum(t, numbers);
+
+        for (uint64_t i = 0; i < result_best.size(); i++)
+        {
+            std::cout << result_best[i] << " ";
+        }
+        
+        std::cout << "\n";
+
+        auto end_bestsum = std::chrono::steady_clock::now();
+
+        std::cout << "Elapsed time was: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_bestsum - start_bestsum).count() << " ms" << std::endl;
 
         std::cout << "Do you want to do another time test? (1 or 0) " << std::endl;
 
